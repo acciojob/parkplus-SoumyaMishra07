b60267316b2c0bc2a5dd96cd91ca9551da703425
SoumyaMishra07 <soumyarmishra065@gmail.com>
2024-03-31 14:06:36 +0530

    initial changes

--- src/main/java/com/driver/repository/ReservationRepository.java
+++ src/main/java/com/driver/repository/ReservationRepository.java
@@ -1,5 +1,7 @@
+
 package com.driver.repository;
 
+import com.driver.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
